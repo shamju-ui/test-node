3bdd7f48a6ac99690e0dc6f268562e525f0414e8
shamju-ui <143580019+shamju-ui@users.noreply.github.com>
2024-10-17 10:00:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+const https = require('https');
+const fs = require('fs');
 const express = require('express');
 const app = express();
+
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Welcome to My Express App!');
-});
+const options = {
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+};
 
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server running at http://localhost:${port}`);
+https.createServer(options, app).listen(port, () => {
+  console.log(`Server running at https://localhost:${port}`);
 });
