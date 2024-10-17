84da8eac4c7ec41469c7ebacca47c4d13179e5ae
shamju-ui <143580019+shamju-ui@users.noreply.github.com>
2024-10-17 09:46:09 +0530

    Update index.js
    change

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Welcome to My Express App!');
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running at http://localhost:${port}`);
 });
